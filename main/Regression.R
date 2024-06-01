6aa4f5ae32baf70726ef7cf7948571508bc2b5ee
beasisti <bbeatricesisti@gmail.com>
2024-06-01 18:53:40 +0200

    lag regression

--- main/Regression.R
+++ main/Regression.R
@@ -8,7 +8,8 @@ library(insight)
 library(lattice)
 library(lme4)
 library(lubridate)
-
+library(tidyr)
+library(plm)
 
 # Creating Dataset for Regression -------------------------------------------------------------------
 
@@ -67,6 +68,11 @@ data <- data %>%
 data <- data %>%
   mutate_at(vars(21:22), as.factor)
 
+# add column for lag regression 
+data <- data %>%
+  arrange(Time) %>% # Assicurati che i dati siano ordinati per il tempo
+  mutate(Vendite.in.Volume.Settimana.Precedente = lag(Vendite.in.Volume, default = 0))
+
 
 # Correlation plot -------------------------------------------------------------------
 
@@ -85,6 +91,10 @@ ggplot(data, aes(x = Vendite.in.Volume)) +
   geom_histogram(binwidth = 1000, fill = "grey", color = "skyblue") +
   labs(title = "Distribuzione delle Vendite", x = "Vendite in Valore", y = "Frequenza")
 
+ggplot(data, aes(x = Vendite.in.Volume.Settimana.Precedente)) + 
+  geom_histogram(binwidth = 1000, fill = "grey", color = "skyblue") +
+  labs(title = "Distribuzione delle Vendite", x = "Vendite in Valore", y = "Frequenza")
+
 hist(data$Prezzo_Sconto, prob = T, xlab = 'Prezzo con Sconto')
 hist(data$Prezzo_NoSconto, prob = T, xlab = 'Prezzo senza Sconto')
 
@@ -92,6 +102,11 @@ hist(data$Prezzo_NoSconto, prob = T, xlab = 'Prezzo senza Sconto')
 data <- data %>%
   mutate(Vendite.in.Volume.log = log(Vendite.in.Volume),
          Prezzo_Sconto.log = log(Prezzo_Sconto))
+
+data <- data %>%
+  mutate(Vendite.in.Volume.Settimana.Precedente.log = ifelse(Vendite.in.Volume.Settimana.Precedente != 0,
+                                                             log(Vendite.in.Volume.Settimana.Precedente),
+                                                             0))
 
 
 # Linear regression for Volume Sales -------------------------------------------------------------------
@@ -132,15 +147,16 @@ plot(model.3)
 model.4 <- lm(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
                  Sconto.Solo.Volantino + 
                  Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
-                 is_summer + Year + Prezzo_NoSconto:as.factor(Product), data = data)
-summary(model.4)
-# 
-volume.lag <- 
+                 is_summer + Year, data = data)
+summary(model.4) # 0.443
+
+
+ 
 model.5 <- plm(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
                 Sconto.Solo.Volantino + 
                 Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
                 is_summer + Year, data = data, index = c("Product"))
-summary(model.5) # 0.443
+summary(model.5) 
 y <- as.numeric(fitted(model.5))
 res <- as.numeric(residuals(model.5))
 plot(y,res)
@@ -152,6 +168,7 @@ leverage <- hatvalues(model.4)
 plot(leverage)
 data.1 <- data[leverage <= 0.015, ]
 # non riesco a fittare il modello model.4
+
 
 # Prediction
 
@@ -171,214 +188,16 @@ ggplot(data.brand, aes(x = Time)) +
   theme_minimal()
 
 
-# Linear Mixed Model for Volume Sales (Brand) -------------------------------------------------------------------
+# Lag regression for Volume Sales -------------------------------------------------------------------
 
-ggplot(data = data, aes(x = as.factor(Product), y = Vendite.in.Volume, fill = as.factor(Product))) +
-  geom_boxplot() +
-  labs(x = 'Brand', y = 'Volumes') +
-  ggtitle('Boxplot of Volume Sales among Brands') +
-  theme_minimal() +
-  theme(axis.text = element_text(size=rel(0.2)), axis.title = element_text(size=rel(1)),
-        plot.title = element_text(face="bold", size=rel(1.75)), legend.text = element_text(size=rel(1.15)),
-        legend.position = 'none')
+model.4 <- lm(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
+                Sconto.Solo.Volantino + 
+                Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
+                is_summer + Year + Vendite.in.Volume.Settimana.Precedente.log, data = data)
+summary(model.4)
 
-ggplot(data = data, aes(x = as.factor(Brand), y = Vendite.in.Volume, fill = as.factor(Brand))) +
-  geom_boxplot() +
-  labs(x = 'Brand', y = 'Volumes') +
-  ggtitle('Boxplot of Volume Sales among Brands') +
-  theme_minimal() +
-  theme(axis.text = element_text(size=rel(0.2)), axis.title = element_text(size=rel(1)),
-        plot.title = element_text(face="bold", size=rel(1.75)), legend.text = element_text(size=rel(1.15)),
-        legend.position = 'none')
-
-boxplot(model.4$residuals ~ data$Product, col = 'orange', xlab = 'Brand', ylab = 'Residuals')
-boxplot(model.4$residuals ~ data$Brand, col = 'orange', xlab = 'Brand', ylab = 'Residuals')
-# residuals differ a lot across brands and products
-
-# random intercept
-lmm.0 = lmer(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
-               Sconto.Solo.Volantino + 
-               Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
-               is_summer + (1 |Brand), data = data)
-summary(lmm.0)
-
-# fixed effects and 95% CIs
-confint(lmm.0, oldNames=TRUE)
-fixef(lmm.0)
-
-# random effects: b_0i
-ranef(lmm.0)
-
-# random intercepts and fixed slopes: (beta_0+b_0i, beta_1, beta_2, beta_3)
-coef(lmm.0)
-head(coef(lmm.0)$Brand)
-
-# let's plot all the regression lines
-plot(data$Prezzo_NoSconto, data$Vendite.in.Volume.log, col = 'grey',
-     main = 'Data and regression lines')          
-for(i in 1:12){
-  abline(coef(lmm.0)$Brand[i,1], coef(lmm.0)$Brand[i,2])
-}
-
-
-
-# Diagnostic plots 
-# 1) Assessing Assumption on the within-group errors
-plot(lmm.0)
-
-qqnorm(resid(lmm.0))
-qqline(resid(lmm.0), col='red', lwd=2)
-
-# 2) Assessing Assumption on the Random Effects
-qqnorm(unlist(ranef(lmm.0)$Brand), main='Normal Q-Q Plot - Random Effects for Brand')
-qqline(unlist(ranef(lmm.0)$Brand), col='red', lwd=2)
-
-
-# Prediction
-predict_re <- predict(lmm.0)
-head(predict_re)
-
-
-# random slope
-lmm.1 = lmer(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
-               Sconto.Solo.Volantino + 
-               Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
-               is_summer + (1 + Prezzo_NoSconto|Brand), data = data)
-summary(lmm.1)
-
-confint(lmm.1, oldNames=TRUE)
-
-# Fixed effects: (beta_0, beta_1, beta_2, beta_3)
-fixef(lmm.1)
-
-# Random effects: (b_0i, b_3i) 
-ranef(lmm.1)
-head(ranef(lmm.1)$Brand)
-
-dotplot(ranef(lmm.1))
-
-# Random intercepts and slopes: (beta_0+b_0i, beta_1, beta_2+b_2i)
-coef(lmm.1)
-head(coef(lmm.1)$Brand)
-
-# Let's plot all the regression lines
-
-plot(data$Prezzo_NoSconto, data$Vendite.in.Volume.log, col='blue',
-     xlab='escs', ylab='achievement',main='Data and regression lines for females')
-for(i in 1:12){
-  abline(coef(lmm.1)$Brand[i,1], coef(lmm.1)$Brand[i,2])
-}
-
-# Diagnostic plots 
-# 1) Assessing Assumption on the within-group errors
-plot(lmm.1)
-
-qqnorm(resid(lmm.1))
-qqline(resid(lmm.1), col='red', lwd=2)
-
-# 2) Assessing Assumption on the Random Effects
-qqnorm(unlist(ranef(lmm.1)$Brand[1]), main='Normal Q-Q Plot - Random Effects on Intercept')
-qqline(unlist(ranef(lmm.1)$Brand[1]), col='red', lwd=2)
-qqnorm(unlist(ranef(lmm.1)$Brand[2]), main='Normal Q-Q Plot - Random Effects on escs')
-qqline(unlist(ranef(lmm.1)$Brand[2]), col='red', lwd=2)
-
-plot(ranef(lmm.1))
-
-
-# Comparing models 
-anova(lmm.0, lmm.1)
-
-# the p-value for the test is essentially zero -> we prefer lmm.1
-
-
-
-# random intercept
-lmm.00 = lmer(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
-               Sconto.Solo.Volantino + 
-               Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
-               is_summer + (1 |Product), data = data)
-summary(lmm.00)
-
-# fixed effects and 95% CIs
-confint(lmm.00, oldNames=TRUE)
-fixef(lmm.00)
-
-# random effects: b_0i
-ranef(lmm.00)
-
-# random intercepts and fixed slopes: (beta_0+b_0i, beta_1, beta_2, beta_3)
-coef(lmm.00)
-head(coef(lmm.00)$Product)
-
-# let's plot all the regression lines
-plot(data$Prezzo_NoSconto, data$Vendite.in.Volume.log, col = 'grey',
-     main = 'Data and regression lines')          
-for(i in 1:12){
-  abline(coef(lmm.00)$Product[i,1], coef(lmm.00)$Product[i,2])
-}
-
-# Diagnostic plots 
-# 1) Assessing Assumption on the within-group errors
-plot(lmm.00)
-
-qqnorm(resid(lmm.00))
-qqline(resid(lmm.00), col='red', lwd=2)
-
-# 2) Assessing Assumption on the Random Effects
-qqnorm(unlist(ranef(lmm.00)$Product), main='Normal Q-Q Plot - Random Effects for Brand')
-qqline(unlist(ranef(lmm.00)$Product), col='red', lwd=2)
-
-
-# Prediction
-predict_re <- predict(lmm.00)
-head(predict_re)
-
-
-# random slope
-lmm.11 = lmer(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
-               Sconto.Solo.Volantino + 
-               Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
-               is_summer + (1 + Prezzo_NoSconto|Product), data = data)
-summary(lmm.11)
-
-confint(lmm.11, oldNames=TRUE)
-
-# Fixed effects: (beta_0, beta_1, beta_2, beta_3)
-fixef(lmm.11)
-
-# Random effects: (b_0i, b_3i) 
-ranef(lmm.11)
-head(ranef(lmm.11)$Product)
-
-dotplot(ranef(lmm.11))
-
-# Random intercepts and slopes: (beta_0+b_0i, beta_1, beta_2+b_2i)
-coef(lmm.11)
-head(coef(lmm.11)$Product)
-
-# Let's plot all the regression lines
-
-plot(data$Prezzo_NoSconto, data$Vendite.in.Volume.log, col = 'blue', main = 'Data and regression lines for females')
-for(i in 1:12){
-  abline(coef(lmm.11)$Product[i,1], coef(lmm.11)$Product[i,2])
-}
-
-# Diagnostic plots 
-# 1) Assessing Assumption on the within-group errors
-plot(lmm.11)
-
-qqnorm(resid(lmm.11))
-qqline(resid(lmm.11), col='red', lwd=2)
-
-# 2) Assessing Assumption on the Random Effects
-qqnorm(unlist(ranef(lmm.11)$Product[1]), main='Normal Q-Q Plot - Random Effects on Intercept')
-qqline(unlist(ranef(lmm.11)$Product[1]), col='red', lwd=2)
-qqnorm(unlist(ranef(lmm.11)$Product[2]), main='Normal Q-Q Plot - Random Effects on escs')
-qqline(unlist(ranef(lmm.11)$Product[2]), col='red', lwd=2)
-
-plot(ranef(lmm.11))
-
-
-# Comparing models 
-anova(lmm.00, lmm.11)
-# the p-value for the test is essentially zero -> we prefer lmm.1
+model.5 <- plm(Vendite.in.Volume.log ~ Prezzo_NoSconto + Prezzo_Sconto.log +
+                 Sconto.Solo.Volantino + 
+                 Sconto.Solo.Display + Sconto.Solo.Riduzione.Prezzo + 
+                 is_summer + Year, data = data, index = c("Product"))
+summary(model.5) 
